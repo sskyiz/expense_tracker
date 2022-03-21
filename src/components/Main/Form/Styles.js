dab1d16da1d7f4612d6d6756e2eb83fa61fd1b6d
sskyiz <ysourabh864@yahoo.com>
2022-03-22 00:39:51 +0530

    Initial

---
+++ src/components/Main/Form/Styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/styles";
+
+export default makeStyles(() => ({
+    radioGroup: {
+        display: 'flex',
+        justifyContent: 'center',
+        marginBottom: '-10px',
+    },
+    button: {
+        marginTop: '20px',
+    }
+}))
